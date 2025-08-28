20a48d6ee5dd5763a41692f4b66dfe4857138469
Harry Moldovanis <hmoldo@gmail.com>
2025-08-28 19:46:46 +0200

    chore: remove unused import

--- apps/react-app/src/features/articles/ArticleDetails.tsx
+++ apps/react-app/src/features/articles/ArticleDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import type { Article } from './articlesSlice';
